935859276c701c26c1a94b063df8264b4fda3b24
Viktor von Prouss <vvpm@icloud.com>
2023-02-19 02:00:09 +0100

    Created bot project

---
+++ Myfirstbot2/Program.cs
@@ -0,0 +1,39 @@
+﻿using DSharpPlus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uwu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            Config.LoadConfig();
+
+            DiscordClient discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = Config.token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("uwu"))
+                    await e.Message.RespondAsync("owo");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+    }
+}
---
+++ Myfirstbot2/config.cs
@@ -0,0 +1,37 @@
+﻿using Myfirstbot2.Properties;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Resources;
+using System.Text;
+using YamlDotNet.Serialization;
+
+namespace uwu
+{
+    internal static class Config
+    {
+        internal static string token = "";
+
+        public static void LoadConfig()
+        {
+            // Writes default config to file if it does not already exist
+            if (!File.Exists("./config.yml"))
+            {
+                File.WriteAllText("./config.yml", Encoding.UTF8.GetString(Resources.default_config));
+            }
+
+            // Reads config contents into FileStream
+            FileStream stream = File.OpenRead("./config.yml");
+
+            // Converts the FileStream into a YAML object
+            IDeserializer deserializer = new DeserializerBuilder().Build();
+            object yamlObject = deserializer.Deserialize(new StreamReader(stream));
+
+            // Converts the YAML object into a JSON object as the YAML ones do not support traversal or selection of nodes by name 
+            ISerializer serializer = new SerializerBuilder().JsonCompatible().Build();
+            JObject json = JObject.Parse(serializer.Serialize(yamlObject));
+
+            // Sets up the bot
+            token = json.SelectToken("bot.token").Value<string>() ?? "";
+        }
+    }
+}
